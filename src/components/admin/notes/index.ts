89005c8907b1c9c249d29d04442e868a7a88d10d
Arthur Mafra <artmafra@gmail.com>
2026-03-02 09:39:56 -0300

    notes deleted

--- src/components/admin/notes/index.ts
+++
@@ -1,4 +0,0 @@
-export { TagsInput } from "./tags-input";
-export { NoteCard } from "./note-card";
-export { NoteFormDialog, NOTE_COLORS } from "./note-form-dialog";
-export { NotesListView, type NotesListViewProps } from "./notes-list-view";
